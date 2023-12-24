2be6cee3acacd9f17c2ec605cbca9c6f2ec91251
your name <you@exemple.com>
2023-12-24 16:19:57 +0700

    CRUD damagereport

--- GIS/Controllers/DamageReportController.cs
+++ GIS/Controllers/DamageReportController.cs
@@ -1,6 +1,9 @@
 ﻿using GIS.Models;
+using GIS.Services.ImplementServices;
 using GIS.Services.InterfaceServices;
+using GIS.ViewModels.BodyMaterial;
 using GIS.ViewModels.DamageReport;
+using GIS.ViewModels.Material;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GIS.Controllers
@@ -15,7 +18,22 @@ namespace GIS.Controllers
         {
             _damageReportService = damageReportService;
         }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _damageReportService.ReadAllAsync(e => true));
+        }
 
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var result = await _damageReportService.ReadByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] AddDamageReport addDamageReport)
         {
@@ -29,6 +47,56 @@ namespace GIS.Controllers
             };
 
             return Ok(await _damageReportService.CreateAsync(damageReport));
+        }
+        // api này dùng để thêm damegereport với date tự nhập theo dạng chuỗi như này: 2021-10-08T04:50:12.0000000
+        [HttpPost("try")]
+        public async Task<IActionResult> Post2([FromBody] AddDamageReport2 addDamageReport2)
+        {
+            DateTime utcDateTime = DateTimeOffset.Parse(addDamageReport2.Date).UtcDateTime;
+            DamageReport damageReport = new()
+            {
+                Date = utcDateTime,
+                Content = addDamageReport2.Content,
+                Cause = addDamageReport2.Cause,
+                BodyId = addDamageReport2.BodyId,
+                AccountId = addDamageReport2.AccountId
+            };
+
+            return Ok(await _damageReportService.CreateAsync(damageReport));
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] AddDamageReport2 updateDamageReport)
+        {
+            DamageReport? damageReport = await _damageReportService.ReadByIdAsync(id);
+            if (damageReport == null)
+            {
+                return NotFound();
+            }
+
+            if (!DateTimeOffset.TryParse(updateDamageReport.Date, out DateTimeOffset parsedDate))
+            {
+                return BadRequest("Invalid date format");
+            }
+
+            damageReport.Date = parsedDate.UtcDateTime;
+            damageReport.Content = updateDamageReport.Content;
+            damageReport.Cause = updateDamageReport.Cause;
+            damageReport.BodyId = updateDamageReport.BodyId;
+            damageReport.AccountId = updateDamageReport.AccountId;
+
+            return Ok(await _damageReportService.UpdateAsync(damageReport));
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            DamageReport? damageReport = await _damageReportService.ReadByIdAsync(id);
+            if (damageReport == null)
+            {
+                return NotFound();
+            }
+            return Ok(await _damageReportService.DeleteAsync(id));
         }
     }
 }
---
+++ GIS/ViewModels/DamageReport/AddDamageReport2.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GIS.ViewModels.DamageReport
+{
+    public class AddDamageReport2
+    {
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public string Cause { get; set; } = string.Empty;
+        [Required]
+        public Guid BodyId { get; set; }
+        [Required]
+        public Guid AccountId { get; set; }
+    }
+}
